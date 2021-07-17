2834b5823df3269b74785785c129379291cd2e70
Nadia Guarracino <nad.guar@gmail.com>
2021-07-17 15:44:11 +0200

    create RecipeContent component

---
+++ src/components/RecipeContent/RecipeContent.tsx
@@ -0,0 +1,29 @@
+import RecipeIngredientsSection from 'components/RecipeIngredientsSection/RecipeIngredientsSection';
+import ReferenceLink from 'components/ReferenceLink/ReferenceLink';
+import { Ingredient } from 'data/recipes/recipes'
+import Subtitle from 'components/Subtitle/Subtitle';
+
+import style from './RecipeContent.module.scss';
+
+interface Props {
+  ingredientsList: Ingredient[];
+  instructionsList: string[];
+  link: string;
+}
+
+const RecipeContent: React.FC<Props> = ({ ingredientsList, instructionsList, link }) => {
+  return (
+    <>
+      <div className={style.ingredientsContainer}>
+        <RecipeIngredientsSection ingredients={ingredientsList} />
+      </div>
+      <div className={style.instructionsContainer}>
+        <Subtitle words="Instructions" />
+        {instructionsList.map((instruction, i) => <span className={style.list}>{instruction}</span>)}
+        <ReferenceLink link={link} />
+      </div>
+    </>
+  );
+};
+
+export default RecipeContent;
