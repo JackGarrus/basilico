9df144a1516cfffa07d8e242e1b9c7f8189b6206
Nadia Guarracino <nad.guar@gmail.com>
2021-08-20 23:33:09 +0200

    add react-select

--- src/components/FiltersContainer/FiltersContainer.tsx
+++ src/components/FiltersContainer/FiltersContainer.tsx
@@ -1,12 +1,25 @@
+import { useHistory } from 'react-router-dom';
+import Select from 'react-select';
+import { mergeAllVegs } from 'data/vegetablesList';
+
 import style from './FiltersContainer.module.scss';
 
 const FiltersContainer: React.FC = () => {
+  const OPTIONS = mergeAllVegs().map((e) => { return { label: e, value: e } })
+
+  let history = useHistory();
+
+  const handleOnChange = (val: any) =>
+    history.push(`/recipes/${val.value}`);
 
   return (
     <div className={style.container}>
       Filtra per...
       <div>
-        <div>select</div>
+        <Select
+          options={OPTIONS}
+          onChange={option => handleOnChange(option)}
+        />
         <div>checkboxes</div>
       </div>
     </div>
