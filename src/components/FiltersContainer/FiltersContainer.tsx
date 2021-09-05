14e49e09929995a9497e8eb0a6a11b12a728c02f
Nadia Guarracino <nad.guar@gmail.com>
2021-09-05 14:31:54 +0200

    [wip] - develop ingredient filter

--- src/components/FiltersContainer/FiltersContainer.tsx
+++ src/components/FiltersContainer/FiltersContainer.tsx
@@ -1,26 +1,34 @@
 import { useHistory } from 'react-router-dom';
 import Select from 'react-select';
 import { mergeAllVegs } from 'data/vegetablesList';
+import { RECIPES } from 'data/recipes';
 
 import style from './FiltersContainer.module.scss';
+import FilterRecipes from 'components/FilterRecipes/FilterRecipes';
 
 const FiltersContainer: React.FC = () => {
   const OPTIONS = mergeAllVegs().map((e) => { return { label: e, value: e } })
+  const mergeAllAllergens = () => [...new Set(RECIPES.map((e: any) => e.recipes).map((j: any) => j.allergens))]
 
-  let history = useHistory();
+  const onSubmit = (data: any) => {
+    console.log(JSON.stringify(data));
+  };
 
   const handleOnChange = (val: any) =>
     history.push(`/recipes/${val.value}`);
-
+  let history = useHistory();
+  const filters = ['vegan', 'vegetarian', 'sweet', 'meat', 'fish']
   return (
     <div className={style.container}>
       Filtra per...
-      <div>
+      <div style={{ maxWidth: '320px' }}>
         <Select
           options={OPTIONS}
           onChange={option => handleOnChange(option)}
         />
-        <div>checkboxes</div>
+        <div>
+          <FilterRecipes />
+        </div>
       </div>
     </div>
   );
