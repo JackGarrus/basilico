d9446a432a0c5b3ca01f8e507e26bd9d35140aa9
Nadia Guarracino <nad.guar@gmail.com>
2021-09-05 14:32:11 +0200

    add util function

--- src/data/vegetablesList.ts
+++ src/data/vegetablesList.ts
@@ -1,7 +1,10 @@
 import { MONTHS } from './monthlyVegs'
+import { RECIPES } from './recipes';
 
 export const mergeAllVegs = () => {
   const FRUITS = [...new Set(MONTHS.data.map((e: any, i: number) => e.fruits).flat())]
   const VEGETABLES = [...new Set(MONTHS.data.map((e: any, i: number) => e.vegetables).flat())]
   return [...FRUITS, ...VEGETABLES]
 }
+
+export const mergeAllAllergens = () => [...new Set(RECIPES.map((e) => e.recipes).map((j) => console.log(j)))]
