a5cd5d53f84349dbcc937f108381a8525f98ac33
Nadia Guarracino <nad.guar@gmail.com>
2021-09-12 22:20:12 +0200

    wip - cleanup customStyles

--- src/components/FiltersContainer/customStyles.ts
+++ src/components/FiltersContainer/customStyles.ts
@@ -6,26 +6,11 @@ const STYLE_VAR = {
 }
 
 export const customStyles = {
-  //control: () => ({}),
-
+  control: () => ({}),
+  option: () => ({}),
   valueContainer: (provided: any, state: any) => ({
     ...provided,
     textTransform: 'capitalize',
-  }),
-
-  control: (provided: any, state: any) => ({
-    ...provided,
-    display: 'flex',
-    width: '100%',
-    boxShadow: 'none',
-    border: 'none',
-    backgroundColor: 'transparent',
-    borderBottom: `2px dashed ${STYLE_VAR.accentColor}`,
-    borderRadius: 0,
-    '&:hover': {
-      borderColor: `${STYLE_VAR.primaryColor}`,
-      cursor: 'pointer'
-    }
   }),
 
   dropdownIndicator: (provided: any, state: any) => ({
@@ -72,26 +57,4 @@ export const customStyles = {
       }
     };
   }
-}
-
-export const customOption = {
-  option: (provided: any, state: any) => ({
-    ...provided,
-    '_inner': {
-      padding: '10px 20px ',
-      backgroundColor: state.isSelected ? `${STYLE_VAR.secondaryColor}` : `${STYLE_VAR.brandColor}`,
-      '_label': {
-        color: '#333',
-        fontWeight: 700,
-        textTransform: 'capitalize',
-        '&:hover': {
-          backgroundColor: `${STYLE_VAR.secondaryColor}`,
-          cursor: 'pointer'
-        },
-        '&:first-letter': {
-          color: `${STYLE_VAR.accentColor}`,
-        }
-      }
-    }
-  }),
 }
