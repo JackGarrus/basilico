936d5b368ded082a1f7a36e2ae5e74a4838df325
Nadia Guarracino <nad.guar@gmail.com>
2021-09-15 23:54:35 +0200

    cleanup methods, change filename

---
+++ src/utils/vegUtils.ts
@@ -0,0 +1,15 @@
+import { MONTHLY_VEGS } from '../data/monthlyVegs'
+import { RECIPES } from '../data/recipes';
+
+export const mergeAllVegs = () => [...getAllFruits(), ...getAllVegs()]
+
+export const filterAllergens = (selectedAllergens: string[]) => {
+  return RECIPES.map(recipesPerIngredient =>
+    recipesPerIngredient.recipes.map((recipe: any) => {
+      if (selectedAllergens.some((selectedAllergene: string) => recipe.allergens.includes(selectedAllergene))) { return null } else return recipe
+    })
+  ).flat()
+}
+
+export const getAllFruits = () => [...new Set(MONTHLY_VEGS.data.map((e: any, i: number) => e.fruits).flat())];
+export const getAllVegs = () => [...new Set(MONTHLY_VEGS.data.map((e: any, i: number) => e.vegetables).flat())];
